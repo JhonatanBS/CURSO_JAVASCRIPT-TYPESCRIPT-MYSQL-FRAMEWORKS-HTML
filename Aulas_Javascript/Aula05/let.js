a5ea7adc0beb7135f9e00dd89f36543de364cd45
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-10-23 16:53:08 -0300

    Função let, serve para atribui valores na variável.

---
+++ Aulas_Javascript/Aula05/let.js
@@ -0,0 +1,21 @@
+/*A função para variáveis, se chama let, com ela conseguimos atribui
+  um valor númerico ou uma cadeia de caracteres*/
+let nome = "Jhonatan";
+console.log(nome);
+nome = "Thiago";
+console.log(nome);// Variáveis variam de acordo com uma atribuição
+/*let nome = 150;//Variáveis podem ser atribuídas dentro do mesmo tipo de dado
+console.log(nome);//Não é aceito mudança do tipo da variável
+*/
+nome = "Thiago";
+let decimal = 3.14;
+console.log(decimal);
+let numero = 100;
+console.log(numero);
+let letra = 'J';
+console.log(letra);
+/*camelCase: Variáveis recebem o nome,caso a variável tenha duas palavras,
+a segunda palavra, a sua inicial tem letra maiúscula*/
+let numeroPrimo = 2;
+console.log(numeroPrimo);
+
