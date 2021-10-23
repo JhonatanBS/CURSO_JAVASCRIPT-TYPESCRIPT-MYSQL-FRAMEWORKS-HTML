febca38a00cdae8707b86827c677572497acc398
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-10-23 15:12:17 -0300

    Aula sobre como inserir comentários no javascript.

---
+++ Aulas_Javascript/Aula03/comentarios.js
@@ -0,0 +1,15 @@
+console.log("Comentários de uma linha são representados por duas barrs '//'");
+//Este é um comentário que o compilador ou motor irá ignorar
+console.log("Comentários como bloco de notas, são representados por '/* */'");
+/*
+Este é um comentário de bloco de notas ou de várias linhas
+O motor ignora essas linhas, desde que estejam dentro da
+barra e o asterisco
+*/
+console.log("Existem comentáios com efeites, mas seguem a regra do segundo comentário");
+/*
+ * Comentário 
+ * para 
+ * documentação
+ * !!!
+ */
