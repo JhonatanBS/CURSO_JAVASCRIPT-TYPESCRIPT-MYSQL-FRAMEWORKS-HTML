7dae026af04d8d19584e7d31da7953e3c717fdd0
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-10-23 11:50:05 -0300

    Método console.log, utilizando strings,números inteiros e decimais.

---
+++ Aulas_Javascript/console.js
@@ -0,0 +1,4 @@
+console.log('Jhonatan') /*String(Aspas simples) = Cadeia de Caracteres*/
+console.log("Jhonatan") /*String(Aspas Duplas) = Cadeia de Caracteres*/
+console.log(`Jesus Cristo`) /*String(Aspas Duplas) = Cadeia de Caracteres*/
+console.log(77,125.5,"Jesus") /*Números inteiros ou Decimais */
