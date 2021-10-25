a3feef40bd0fe84fd6d3bf7a48a86b3b4b51058b
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-10-25 10:17:44 -0300

    Tipos de dados primitivos em javascript.

---
+++ Aulas_Javascript/Aula06/primitivos.js
@@ -0,0 +1,21 @@
+//Tipos de Dados Primitivos
+let dado = 6;
+const pi = 3.14;
+var idade = 28;
+let nome;
+const sobrenome = null;
+const aprovado = true;
+const reprovado = false;
+
+let a = 5;
+const b = a;
+const aluno = "Carlos";
+
+console.log(typeof dado, dado);
+console.log(typeof pi, pi);
+console.log(typeof idade, idade);
+console.log(typeof nome, nome);
+console.log(typeof sobrenome, sobrenome);
+console.log(typeof aprovado, aprovado);
+console.log(typeof reprovado, reprovado);
+console.log(typeof aluno, aluno);
