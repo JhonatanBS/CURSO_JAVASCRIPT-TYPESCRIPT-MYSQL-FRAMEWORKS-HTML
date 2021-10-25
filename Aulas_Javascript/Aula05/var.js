d81155645e00d9b146af4d15555e27f6f4c3d37d
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-10-25 09:25:01 -0300

    Var usado para variáveis que podem ser redeclaradas, e o uso de variável global.

---
+++ Aulas_Javascript/Aula05/var.js
@@ -0,0 +1,8 @@
+/*Var utilizado para variáveis que variam ou podem ser atribuídas novos valores*/
+var nome = "José";
+var nome = "João";
+/*Variável Global*/
+sobrenome = "Batista";
+
+console.log(nome);
+console.log(sobrenome);
