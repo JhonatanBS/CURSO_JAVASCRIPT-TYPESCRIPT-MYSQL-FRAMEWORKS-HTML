de844f748fe8c855ea4a30af3c9353fc440a3069
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-10-24 21:08:43 -0300

    Constantes em Javascript.

---
+++ Aulas_Javascript/Aula05/constante.js
@@ -0,0 +1,17 @@
+// Constantes não podem variar, ou seja, só é possível atribuir uma vez o valor para uma constante
+const par = 81;
+const impar = 9;
+const palavra = "java";
+console.log(par);
+console.log(impar);
+const soma = par + impar;
+const multiplicacao = par * impar;
+const subtracao = par - impar;
+const divisao = par / impar;
+console.log(soma);
+console.log(multiplicacao);
+console.log(subtracao);
+console.log(divisao);
+//Especial: Da para saber o tipo de dado de uma variável ou constante, usando o typeof
+console.log(typeof par);
+console.log(typeof palavra);
