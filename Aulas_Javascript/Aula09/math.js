de1e881f55301bae19d7eabb928d02717d2f581c
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-10-28 09:52:01 -0300

    Biblioteca Math, com diversos atributos para operações matemáticas.

---
+++ Aulas_Javascript/Aula09/math.js
@@ -0,0 +1,16 @@
+/*Variáveis let*/
+let numero1 = 9.68;
+let numero2 = 7.7;
+
+/*Arredonda o número para cima*/
+console.log(Math.ceil(numero1));
+/*Arredonda o número para baixo*/
+console.log(Math.floor(numero1));
+/*Arredonda de forma automática para cima ou para baixo*/
+console.log(Math.round(numero1));
+/*Gera um número aleatório entre 0 e n*/
+const aleatorio = console.log( Math.round(Math.random() * 1000));//round: Arredonda por causa do números com casas decimais
+/*Busca o maior número da lista*/
+console.log(Math.max(1,2,3,4,5,6,10,30,7,15,2.5)); 
+/*Busca o menor número da lista*/ 
+console.log(Math.min(1,2,3,4,5,6,10,30,7,15,2.5)); 
