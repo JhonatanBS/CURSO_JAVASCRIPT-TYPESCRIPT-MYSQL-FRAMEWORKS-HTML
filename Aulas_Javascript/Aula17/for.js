b75a762c315d3a80cf7f0494c2fc50bce7d3310b
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-11-24 09:33:51 -0300

    Laço for para repetições.

---
+++ Aulas_Javascript/Aula17/for.js
@@ -0,0 +1,12 @@
+/*
+Estrutura de Repetição
+
+for(inicio,parada,operação){ Bloco de chaves
+    Repetição
+}
+*/
+const frutas = ["Maça", "Banana", "Melão", "Melancia"];
+
+for (i = 0;i<frutas.length;i++){
+  console.log(frutas[i]);
+}
