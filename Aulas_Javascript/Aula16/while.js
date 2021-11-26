eeff850ad5d2e241ca5045cdd096eed3c90bf01a
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-11-26 08:04:37 -0300

    Laço While.

---
+++ Aulas_Javascript/Aula16/while.js
@@ -0,0 +1,13 @@
+function random( min, max){
+   const r = Math.random() * (max - min) + min;
+   return Math.floor(r);
+}
+
+const min = 1;
+const max = 50;
+let rand;
+/*Enquanto a condição for verdadeira, ela será executada*/
+while(rand !== 10){/*rand diferente de 10*/
+    rand = random(min , max);
+    console.log(rand);
+}
