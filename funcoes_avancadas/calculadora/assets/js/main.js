5fbdaada98857c733c713105088373281b6d1413
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2021-12-02 11:35:23 -0300

    Projeção de calculadora usando Javascript.

---
+++ funcoes_avancadas/calculadora/assets/js/main.js
@@ -0,0 +1,74 @@
+function criaCalculadora(){
+    return {
+
+     display: document.querySelector(".display"),
+     
+     inicia(){
+       this.cliqueBotoes();
+       this.pressionaEnter();
+     },
+
+     pressionaEnter(){
+       this.display.addEventListener("keyup", e =>{
+           if(e.keyCode === 13){
+               this.realizaConta();
+           }
+       });
+     },
+
+     realizaConta(){
+       let conta = this.display.value;
+       
+       try{
+          conta = eval(conta);
+          
+          if(!conta){
+              alert("Conta Inválida!");
+              return;
+          }
+
+          this.display.value = String(conta);
+       }catch(e){
+        alert("Conta Inválida!");
+        return;
+       }
+     },
+
+     clearDisplay(){
+       this.display.value = "";
+     },
+
+     apagaUm(){
+       this.display.value = this.display.value.slice(0,-1);
+     },
+
+     cliqueBotoes(){
+         document.addEventListener("click",(e) =>{
+         const el = e.target;
+         console.log(this);
+         if(el.classList.contains("btn-num")){
+             this.btnParaDisplay(el.innerText);
+         }
+
+         if(el.classList.contains("btn-clear")){
+            this.clearDisplay(el.innerText);
+        }
+
+        if(el.classList.contains("btn-del")){
+            this.apagaUm();
+        }
+
+        if(el.classList.contains("btn-eq")){
+            this.realizaConta();
+        }
+     });
+     },
+
+     btnParaDisplay(valor){
+         this.display.value += valor;
+     }
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
