2c367406ed980e8d10551f6ba5c0bf2d8513eaab
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-01-15 14:51:48 -0300

    Promise com os métodos all, race, resolve e reject.

--- JavaScript/assincrono/promise.js
+++ JavaScript/assincrono/promise.js
@@ -1,6 +1,6 @@
 /*
 Promise: Um objeto usado para processamento assíncrono, possuem três estados:
-         Pendente, rejeitada e realizada, com os métods then e catch.
+         Pendente, rejeitada e realizada, com os métodos then e catch.
 */
 
 function rand(min, max){
@@ -34,3 +34,29 @@ esperaAi("Conexão com O BD",rand(1,3))
    .catch(e =>{
        console.log("Erro:", e);
    });
+
+   const promises = [
+    //    "Primeiro valor",
+       esperaAi("Promise 1", 3000),
+       esperaAi("Promise 2", 500),
+       esperaAi("Promise 1", 1000),
+    //    "Outro valor"
+   ];
+
+// Promise.all => Executa todas as itens da variável promises
+Promise.all(promises)
+.then(function(valor){
+    console.log(valor);
+})
+.catch(function(erro){
+    console.log(erro);
+});
+
+// Promise.race => Executa o primeiro promise que ganhar
+Promise.race(promises)
+.then(function(valor){
+    console.log(valor);
+})
+.catch(function(erro){
+    console.log(erro);
+});
